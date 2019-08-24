1bf94dc6d71fff62aba0e39395d6542d02652270
Prashant1019 <54199276+Prashant1019@users.noreply.github.com>
2019-08-24 16:17:36 +0530

    Add files via upload

---
+++ startup.js
@@ -0,0 +1,14 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+// var backg = document.querySelector("#filter_change")
+
+// backg.addEventListener("click",function(){
+// 	filter_change.style.background = filter_change.classList.add("back")
+// 	// filter_change.classList.add("back")
+// })
